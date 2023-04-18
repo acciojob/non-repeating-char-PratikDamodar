b361e9c570e080f5cb91cff335c2328684359b2b
PratikDamodar <117561545+PratikDamodar@users.noreply.github.com>
2023-04-18 16:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function firstNonRepeatingChar(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+      return str[i];
+    }
+  }
+  return null;
+}
+
+let str = prompt("Enter a string:");
+let result = firstNonRepeatingChar(str);
+if (result) {
+  alert("The first non-repeating character is: " + result);
+} else {
+  alert("No non-repeating character found.");
+}
